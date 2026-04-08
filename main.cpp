6dcbcf79fb7ea5952a1ed9fe5edf074acf4d59e2
JoeFrumenti <joefrumenti@gmail.com>
2026-04-08 14:55:39 -0700

    added uniform that gradually changes color

--- main.cpp
+++ main.cpp
@@ -11,9 +11,10 @@ const char* vertexShaderSource = "#version 330 core\n"
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"uniform vec4 ourColor;\n"
 "void main()\n"
 "{\n"
-" FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+" FragColor = ourColor;\n"
 "}\0";
 
 float vertices[] = {
@@ -158,11 +159,23 @@ int main()
 	{
 		processInput(window);
 
+
+
+
 		//render loop
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		glUseProgram(shaderProgram);
+		
+		
 		glBindVertexArray(VAO);
+
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUseProgram(shaderProgram);
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
