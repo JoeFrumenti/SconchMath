f9065da620f47fa5ab726dbb995277f34fb7106c
JoeFrumenti <joefrumenti@gmail.com>
2026-04-09 10:47:52 -0700

    LAID HIM FLAT

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <Shaders/shader.h>
 #include <Textures/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 float offset = 0;
 
@@ -153,6 +156,26 @@ int main()
 	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0); // manually
 	ourShader.setInt("texture2", 1); // or with shader class
 
+
+	//transformations
+
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	glm::mat4 view = glm::mat4(1.0f);
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f,
+		100.0f);
+
+	int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+	int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
+	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+	
+
 	while (!glfwWindowShouldClose(window))
 	{
 		// input
@@ -164,11 +187,10 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// bind Texture
-		
 
 		// render container
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
