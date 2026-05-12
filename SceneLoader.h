159fa62424b1e155a0f1395adeb47447fd11e030
JoeFrumenti <joefrumenti@gmail.com>
2026-05-11 20:45:10 -0600

    moved scene setup to sceneloader

---
+++ SceneLoader.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "coin.h"
+#include "BouncingCube.h"
+#include "BasicModel.h"
+#include "DebugCube.h"
+#include "Player1.h"
+#include "Player2.h"
+#include "CoinPickup.h"
+#include "DropCoins.h"
+#include "PlayerManager.h"
+#include "Icey.h"
+#include "Launcher.h"
+#include "SlimeBall.h"
+
+class SceneLoader {
+public:
+	void loadFightScene();
+	void initFightScene();
+};
--- main.h
+++ main.h
@@ -17,18 +17,7 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
-#include "coin.h"
-#include "BouncingCube.h"
-#include "BasicModel.h"
-#include "DebugCube.h"
-#include "Player1.h"
-#include "Player2.h"
-#include "CoinPickup.h"
-#include "DropCoins.h"
-#include "PlayerManager.h"
-#include "Icey.h"
-#include "Launcher.h"
-#include "SlimeBall.h"
+#include "SceneLoader.h"
 
 #include "MyTimer.h"
 #include "Window.h"
@@ -55,8 +44,7 @@ const double TARGET_FPS = 60.0;
 const std::chrono::duration<double> FRAME_DURATION(1.0 / TARGET_FPS);
 
 int started = 0;
-BouncingCube* cube1;
-BouncingCube* cube2;
+SceneLoader sceneLoader = SceneLoader();
 
 
 void renderLoop() {
@@ -99,8 +87,7 @@ void renderLoop() {
 }
 
 void init() {
-    cube1->setVelocity(glm::vec3(0.10f, -0.21f, .0f));
-    cube2->setVelocity(glm::vec3(-0.23f, 0.08f, .0f));
+    
     start = true;
     SoundManager::getInstance().playImportantSound("announce");
 }
@@ -129,86 +116,5 @@ void setupWindow() {
 }
 
 void addGameObjects() {
-    //GameObject setup & config
-    /*DebugCube* dc = new DebugCube();
-    dc->setId(9);
-    dc->setScale(glm::vec3(0.1, .1, .1));
-    UDMan.addUD(dc);*/
-
-
-    cube1 = new BouncingCube("C:/Users/joefr/source/repos/SconchMath/assets/Models/DiamondSphere.obj");
-    cube1->translate(glm::vec3(5.0f, .0f, 0.0f));
-    cube1->addTag("bcube");
-
-    Player1* p1 = new Player1();
-    CoinPickup* coinPickup = new CoinPickup();
-    SlimeBall* slimeBall = new SlimeBall();
-
-    cube1->addChild(coinPickup);
-    cube1->addChild(p1);
-    UDMan.addUD(cube1);
-
-
-
-    cube2 = new BouncingCube("C:/Users/joefr/source/repos/SconchMath/assets/Models/DiamondSphere2.obj");
-    cube2->translate(glm::vec3(-5.0f, .0f, 0.0f));
-    cube2->addTag("bcube");
-
-    Player2* p2 = new Player2();
-    CoinPickup* coinPickup2 = new CoinPickup();
-    Icey* icey = new Icey();
-    cube2->addChild(p2);
-    cube2->addChild(coinPickup2);
-    cube2->addChild(icey);
-    UDMan.addUD(cube2);
-
-
-    /* BouncingCube* moneyBag = new BouncingCube("C:/Users/joefr/source/repos/SconchMath/assets/Models/StarCube.obj");
-     moneyBag->translate(glm::vec3(.0f, 5.0f, 0.0f));
-     moneyBag->setVelocity(glm::vec3(-0.45f, 0.16f, .0f));
-     DropCoins* dropCoins = new DropCoins();
-     moneyBag->addChild(dropCoins);*/
-
-
-
-
-     //    UDMan.addUD(moneyBag);
-
-
-    BasicModel* bg = new BasicModel("C:/Users/joefr/source/repos/SconchMath/assets/Models/backgroundPB.obj");
-    BasicModel* foreground = new BasicModel("C:/Users/joefr/source/repos/SconchMath/assets/Models/backgroundBW.obj");
-
-
-
-    //foreground->setInput(true);
-    foreground->translate(glm::vec3(0.025f, 1.2f, -1.0f));
-    foreground->rotate(3.14159265358979 / 2, glm::vec3(.0f, 1.0f, 0.0f));
-    foreground->scale(glm::vec3(4.54999f, 4.81, 4.54999f));
-    UDMan.addUD(foreground);
-
-    bg->translate(glm::vec3(0.0f, -10.0f, -3.0f));
-
-    bg->rotate(3.14159265358979f, glm::vec3(.0f, .0f, 1.0f));
-
-    UDMan.addUD(bg);
-
-
-    PlayerManager* playerMan = new PlayerManager(cube1, cube2);
-    UDMan.addUD(playerMan);
-
-
-    Launcher* slimeLauncher = new Launcher(slimeBall, playerMan);
-    cube1->addChild(slimeLauncher);
-
-
-
-    //SPAWN COINS
-    int idNum = 10000;
-    for (float i = -5.5f; i <= 5.5f; i += 1.2) {
-        for (float j = -5.0f; j <= 7.5f; j += 1.35f) {
-            Coin* coiny = new Coin();
-            coiny->translate(glm::vec3(i, j, 0));
-            UDMan.addUD(coiny);
-        }
-    }
+    sceneLoader.loadFightScene();
 }
